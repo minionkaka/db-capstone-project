e2153c900aff7bb15c67ead96e54b54547362493
minionkaka <128003953+minionkaka@users.noreply.github.com>
2023-10-13 12:18:34 +0800

    Create week2-UpdateBooking.sql

---
+++ week2-UpdateBooking.sql
@@ -0,0 +1,19 @@
+DROP PROCEDURE IF EXISTS UpdateBooking;
+
+DELIMITER //
+
+CREATE PROCEDURE UpdateBooking(
+  IN booking_id INT, 
+  IN booking_date DATE)
+BEGIN
+
+UPDATE Bookings SET Date = booking_date
+WHERE BookingID = booking_id;
+
+SELECT CONCAT('Booking ', booking_id, ' updated') AS 'Confirmation';
+
+END //
+
+DELIMITER ;
+
+CALL UpdateBooking(9, "2022-12-17")
